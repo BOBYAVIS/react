e1e1bad2a518bd4a1f04b641ea52ec902108bfd5
BOBYAVIS <robby60.rb@gmail.com>
2023-05-14 00:54:38 -0700

    INITIAL COMMIT BY BOBYAVIS

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function WelcomeHome() {
+  return <p>welcome, feel at home.</p>
+}
+
+function WelcomeMessage() {
+  return <p>Welcome!</p>
+  }
+
+function App() {
+  return (
+    <div className="App">
+      HELLO WORLD
+    </div>
+  );
+}
+
+export default App;
